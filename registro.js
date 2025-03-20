048d8a779ec79c9bbd8c5b94f2ceb62dc4b4820f
Vyneelric <vini.cardoso1909@gmail.com>
2025-03-20 12:04:29 -0300

    cadastro feito

--- registro.js
+++ registro.js
@@ -5,13 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
     // Botão de registro chamado pelo ID
     const button = document.getElementById("button");
 
-    function pegarDadosLogin() {
+    function pegarDadosCadastro() {
         // Chamando os elementos do registro pelo ID
+        const nome = document.getElementById("name").value;
         const email = document.getElementById("email").value;
         const senha = document.getElementById("password").value;
+        const img = document.getElementById("img").value;
+        const checkbox = document.getElementById("checkbox").checked ? 1 : 0;
 
         // Verifica se todos os campos obrigatórios estão preenchidos
-        if (!email || !senha) {
+        if (!nome || !email || !senha || !img) {
             alert("Todos os campos são obrigatórios!");
             return null;
         }
@@ -25,35 +28,35 @@ document.addEventListener("DOMContentLoaded", function () {
         };
     }
 
-    async function loginUsuario() {
+    async function cadastrarUsuario() {
         try {
-            const dadosLogin = pegarDadosLogin();
+            const dadosCadastro = pegarDadosCadastro();
 
-            if (!dadosLogin) return; // Interrompe a função se os dados forem inválidos
+            if (!dadosCadastro) return; // Interrompe a função se os dados forem inválidos
 
             // Envia os dados para a API usando o método POST
-            const response = await fetch("https://back-spider.vercel.app/login", {
+            const response = await fetch("https://back-spider.vercel.app/user/cadastrarUser", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify(dadosLogin),
+                body: JSON.stringify(dadosCadastro),
             });
 
             // Converte a resposta para JSON
             const result = await response.json();
 
             // Verifica se o cadastro foi bem-sucedido
-            if (response.status === 200) {
-                alert("Login realizado com sucesso!");
+            if (response.status === 201) {
+                alert("Cadastro realizado com sucesso!");
             } else {
                 alert(`Erro: ${result.message || "Algo deu errado"}`);
             }
         } catch (error) {
             console.error("Erro ao fazer a requisição:", error);
-            alert("Ocorreu um erro ao tentar logar.");
+            alert("Ocorreu um erro ao tentar cadastrar o usuário.");
         }
     }
 
-    button.addEventListener("click", loginUsuario());
+    button.addEventListener("click", cadastrarUsuario);
 });
